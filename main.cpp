5b1beaed70cb1c8162097a82bdbcf2ee86212066
keno555552 <kwankinleung5@gmail.com>
2025-06-27 11:25:20 +0900

    加入專案檔案。

---
+++ main.cpp
@@ -0,0 +1,125 @@
+﻿#include <Novice.h>
+#include "myMath.h"
+#include "crashDecision.h"
+#include "ImGui.h"
+
+const char kWindowTitle[] = "GC01_05_カン_ケンリャン";
+
+int kWindowWidth = 1280, kWindowHeight = 720;
+
+///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+/// 重要定義、構造体作り
+///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
+/// 計算に使う値
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+	/// 初期化
+	///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
+	Vector3 controlPoints[3] = {
+		{-0.00f, 0.58f, 1.00f},
+		{ 1.76f, 1.00f,-0.30f},
+		{ 0.94f,-0.70f, 2.30f}
+	};
+
+
+	///カメラ初期化
+	Vector3 cameraTranslate{ 0.0f, 1.9f, -6.49f };
+	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
+
+	Matrix4x4 cameraMatix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+	Matrix4x4 cameraViewMatrix = Inverse(cameraMatix);
+	Matrix4x4 cameraProjectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+	Matrix4x4 cameraWorldViewProjectionMatrix = cameraProjectionMatrix * cameraViewMatrix;
+	Matrix4x4 cameraViewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+		/// ボタン処理
+		///=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+
+		if (keys[DIK_A]) { cameraTranslate.x -= 0.1f; }
+		if (keys[DIK_D]) { cameraTranslate.x += 0.1f; }
+		if (keys[DIK_W]) { cameraTranslate.z += 0.1f; }
+		if (keys[DIK_S]) { cameraTranslate.z -= 0.1f; }
+		if (keys[DIK_Z]) { cameraRotate.y += 0.1f; }
+		if (keys[DIK_C]) { cameraRotate.y -= 0.1f; }
+		if (keys[DIK_R]) {
+			cameraTranslate = { 0.0f, 1.9f, -6.49f };
+			cameraRotate = { 0.26f, 0.0f,   0.0f };
+		}
+
+		///=========================================================================================================================================================================================
+		/// 更新処理
+		///=========================================================================================================================================================================================
+
+		///カメラ更新処理
+		cameraMatix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, cameraRotate, cameraTranslate);
+		cameraViewMatrix = Inverse(cameraMatix);
+		cameraProjectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		cameraWorldViewProjectionMatrix = cameraViewMatrix * cameraProjectionMatrix;
+		cameraViewportMatrix = MakeViewportMatrix(0.0f, 0.0f, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		/// MaxMin制限
+		//if (aabb1.max.x < aabb1.min.x) { aabb1.max.x = aabb1.min.x + 0.01f; }
+		//if (aabb1.max.y < aabb1.min.y) { aabb1.max.y = aabb1.min.y + 0.01f; }
+		//if (aabb1.max.z < aabb1.min.z) { aabb1.max.z = aabb1.min.z + 0.01f; }
+
+		///=========================================================================================================================================================================================
+		/// 描画処理
+		///=========================================================================================================================================================================================
+
+		/// Grid
+		DrawGrid(cameraWorldViewProjectionMatrix, cameraViewportMatrix);
+
+		/// 判定と描画
+		DrawBezier(controlPoints[0], controlPoints[1], controlPoints[2], 20, cameraWorldViewProjectionMatrix, cameraViewportMatrix, BLUE);
+		DrawSphere({ controlPoints[0] ,0.01f}, cameraWorldViewProjectionMatrix, cameraViewportMatrix, RED);
+		DrawSphere({ controlPoints[1] ,0.01f}, cameraWorldViewProjectionMatrix, cameraViewportMatrix, BLACK);
+		DrawSphere({ controlPoints[2] ,0.01f}, cameraWorldViewProjectionMatrix, cameraViewportMatrix, BLUE);
+
+
+		/// ImGui
+		ImGui::Begin("Control Penol");
+		ImGui::SliderFloat3("controlPoints[0]", &controlPoints[0].x, -5.0f, 5.0f);
+		ImGui::SliderFloat3("controlPoints[1]", &controlPoints[1].x, -5.0f, 5.0f);
+		ImGui::SliderFloat3("controlPoints[2]", &controlPoints[2].x, -5.0f, 5.0f);
+		ImGui::End();
+
+
+
+		/// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		/// StageEND
+		/// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
